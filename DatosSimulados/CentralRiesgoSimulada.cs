c12bfb4a1e95e76fa2a6c6f41072901dd6cbceb5
ErikSantiago-G <sg1861111@gmail.com>
2026-09-16 17:14:18 -0500

    Fix: Capa de datos simulados con la inversion de dependencias

--- DatosSimulados/CentralRiesgoSimulada.cs
+++ DatosSimulados/CentralRiesgoSimulada.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using CapaNegocio.Interfaces;
 
 namespace DatosSimulados
 {
-    internal class CentralRiesgoSimulada
+    // Implementa el MISMO contrato que la clase real de AccesoDatos.
+    public class CentralRiesgoSimulada : IAccesoCentralRiesgo
     {
+        private readonly Dictionary<(string tipoDoc, string nroDoc), int> _puntajes;
+
+        public CentralRiesgoSimulada()
+        {
+            _puntajes = new Dictionary<(string, string), int>
+            {
+                { ("CC", "12234587"), 563 },
+                { ("CC", "1000111222"), 820 },
+                { ("CC", "1000333444"), 650 },
+                { ("CC", "1000555666"), 410 },
+            };
+        }
+
+
+
+        // Permite que cada prueba registre el puntaje exacto que necesita para ese escenario, sin tocar los datos precargados de arriba.
+        public void RegistrarPuntaje(string tipoDoc, string nroDoc, int puntaje)
+        {
+            _puntajes[(tipoDoc, nroDoc)] = puntaje;
+        }
+
+
+
+
+        public int ConsultarPuntaje(string tipoDoc, string nroDoc)
+        {
+            return _puntajes.TryGetValue((tipoDoc, nroDoc), out int puntaje) 
+                ? puntaje : 0;
+        }
     }
 }
