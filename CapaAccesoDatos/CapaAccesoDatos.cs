2331e15fff45379c5555a2fc255620379ec8d022
ErikSantiago-G <sg1861111@gmail.com>
2026-09-15 19:24:09 -0500

    Fix: Se inicializa la estructura de la capa de acceso de datos

---
+++ CapaAccesoDatos/CapaAccesoDatos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace CapaAccesoDatos
+{
+    public class CapaAccesoDatos
+    {
+        private SqlConnection conexion;
+
+        public ConexionSQlServer() {
+            this.conexion = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Application Name=\"ParcialCentralRiesgo\";Command Timeout=0;Initial Catalog=BD_Central_Riesgo")
+        }
+        public bool ConsultarPuntaje(string TipoDoc, string NroDoc)
+        {
+            int puntaje = 0;
+            
+            string select = string.Format ("Select * FROM CentralRiesgo WHERE TipoDoc = '{0}' AND NroDoc ='{1}' ", TipoDoc, NroDoc);
+
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand(select, conexion);
+            SqlDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                puntaje = reader.GetInt32(2);
+
+            }
+            conexion.Close();
+        }
+    }
+}
